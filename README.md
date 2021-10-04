1fc952d202ce1ee9de20a0030d7b3de90a7fe1ed
Willian <willianribeirodev@gmail.com>
2021-10-04 12:27:18 -0300

    feat: f

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
-# creative_drive
+<div align="center" id="top"> 
+
+</div>
+
+<h1 align="center">Creative drive</h1>
+
+
+<!-- Status -->
+
+<!-- <h4 align="center"> 
+	🚧  Creative_drive 🚀 Under construction...  🚧
+</h4> 
+
+<hr> -->
+
+<p align="center">
+  <a href="#sobre">Sobre</a> &#xa0; | &#xa0; 
+  <a href="#requiditos">Requisitos</a> &#xa0; | &#xa0;
+  <a href="#tecnologias">Tecnologias</a> &#xa0; | &#xa0;
+  <a href="#iniciando">Iniciando</a> &#xa0; | &#xa0;
+  <a href="https://github.com/Nailliw/" target="_blank">Author</a>
+</p>
+
+<br>
+
+##  Sobre ##
+
+Esse é um teste técnico com o objetivo de verificar o fit técnico para a vaga.
+
+
+
+##  Requisitos ##
+1. Criar um servidor http (flask, django, bottle, etc) contendo 2 serviços:
+
+    - Login (username, password).
+
+        - pode ser hardcoded o usuário/senha.
+
+    - Upload (file)
+
+        - salvar o arquivo no diretório `uploads` junto do código do serviço http python.
+
+
+2. Criar um app escrito em Python utilizando PyQt ou PySide (preferencialmente) contendo as seguintes funcionalidades:
+
+    - Tela de login (consumir o serviço criado anteriormente)
+
+    - Na tela principal:
+
+        - tem que ter um botão para selecionar um arquivo do sistema e outro botão para realizar o upload.
+
+        - tem que ter uma área destinada a lista de arquivos sendo feito o upload, a barra de progresso e uma previsão de término.
+
+        - deverá haver uma opção para cancelar o upload.
+
+    - O aplicativo deverá estar sempre disponível no trackbar do windows (mesmo quando o aplicativo for fechado pela janela principal).
+
+        - deverá haver um menu no trackbar do windows com a possibilidade de fechar o aplicativo.
+
+##  Tecnologias ##
+
+Essas foram as tecnologias utilizadas no projeto:
+
+- [PyQt5](https://www.riverbankcomputing.com/software/pyqt/)
+- [Django](https://www.djangoproject.com)
+
+
+##  Iniciando ##
+
+```bash
+# Clone this project
+$ git clone https://github.com/Nailliw/creative_drive
+
+# Verifique se a API está rodando
+
+# Accesse
+$ cd creative_drive
+
+# Instale as dependências
+$ pip install -r requirements.txt
+
+# Rode o executável
+```
+
+<a href="#top">Back to top</a>
--- login.py
+++ login.py
@@ -1,5 +1,10 @@
+from os import times
 from PyQt5 import uic, QtWidgets
-import requests, json
+import requests
+import time
+import multiprocessing
+
+URL_BASE = "localhost:8000/"
 
 
 def login():
@@ -7,11 +12,28 @@ def login():
     username = login_screen.login_email.toPlainText()
     password = login_screen.login_password.text()
 
-    if username == "will" and password == "1234":
-        login_screen.close()
-        home_screen.show()
+    if username != "" and password != "":
+        #requisicao = requisicao_login(username=username, password=password)
+        requisicao = True
+        if requisicao:
+            login_screen.close()
+            home_screen.show()
+        else:
+            login_screen.error_message.setText("Dados de login incorretos.")
+
     else:
         login_screen.error_message.setText("Dados de login incorretos.")
+
+
+def requisicao_login(username, password):
+    data = data = {"username": username, "password": password}
+    response = requests.post(
+        f'{URL_BASE}/login/', data=data
+    ),
+    if response.status_code == 200:
+        return True
+    else:
+        return False
 
 
 def logout():
@@ -25,20 +47,53 @@ def register():
 
 
 def send_new_register():
-    email = register_screen.email.toPlainText()
+    username = register_screen.email.toPlainText()
     name = register_screen.name.toPlainText()
     password = register_screen.password.text()
     confirmPassword = register_screen.confirm_password.text()
 
-    if password == confirmPassword:
-        try:
-            register_screen.error_message.setText("Usuário cadastrado com sucesso!")
+    if username != "" and password != "" and name != "" and confirmPassword != "":
+        if password == confirmPassword:
+            #requisicao = requisicao_sign(username, password)
+            requisicao = True
+            if requisicao:
+                try:
+                    register_screen.error_message.setText(
+                        "Usuário cadastrado com sucesso!")
 
-            register_screen.close()
-            login_screen.show()
+                    register_screen.close()
+                    login_screen.show()
 
-        except:
-            register_screen.error_message.setText("Erro ao inserir dados.")
+                except:
+                    register_screen.error_message.setText(
+                        "Erro ao inserir dados.")
+    else:
+        register_screen.error_message.setText(
+            "Erro ao inserir dados.")
+
+
+def requisicao_sign(username, password):
+    data = data = {"username": username, "password": password}
+    response = requests.post(
+        f'{URL_BASE}/signup/', data=data
+    ),
+
+    if response.status_code == 201:
+        return response.content
+    else:
+        return response.content
+
+
+def requisicao_upload(self, name, file):
+
+    response = requests.post(
+        f'{URL_BASE}/users/', files=dict(foo=file)
+    ),
+
+    if response.status_code == 201:
+        return response.content
+    else:
+        return response.content
 
 
 app = QtWidgets.QApplication([])
